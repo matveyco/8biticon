d985c137c46b52c21d12f8eb63adb1b2ffcd1dea
Monday Freelem <mfthemon@gmail.com>
2012-12-19 16:54:48 +0200

    Initial commit

--- 8bit-server/python-django/constructor/views.py
+++ 8bit-server/python-django/constructor/views.py
@@ -1,14 +1,39 @@
+import socket
+origGetAddrInfo = socket.getaddrinfo
+
+def getAddrInfoWrapper(host, port, family=0, socktype=0, proto=0, flags=0):
+    return origGetAddrInfo(host, port, socket.AF_INET, socktype, proto, flags)
+
+# replace the original socket.getaddrinfo by our version
+socket.getaddrinfo = getAddrInfoWrapper
+
+
 import mimetypes
 import os
 import urllib
+import urllib2
 import uuid
+import facebook
+from poster.encode import multipart_encode
+from poster.streaminghttp import register_openers
 
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 from django.utils import simplejson
+from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 from constructor.utils import compose_image
 
+
+__author__ = 'LittleJoe'
+
+def constructor(request):
+    return render_to_response('constructor/constructor.html',
+        {'VK_APP_ID': settings.VK_APP_ID,
+         'FB_APP_ID': settings.FB_APP_ID},
+        context_instance=RequestContext(request))
 
 @csrf_protect
 def download_masterpiece(request):
@@ -54,3 +79,69 @@ def download_masterpiece(request):
 
     file.close()
     return response
+
+
+@never_cache
+def set_vk(request):
+    upload_url = request.POST.get('upload_url')
+
+    if upload_url:
+        data = request.POST.get('data')
+
+        try:
+            data = simplejson.loads(data)
+        except ValueError:
+            data = None
+
+        gender = request.POST.get('gender')
+        image = compose_image(data, gender)
+
+        image_filename = "%s.%s" % (str(uuid.uuid4()), 'png')
+        image_path = "%s/%s" % (settings.MEDIA_ROOT, image_filename)
+        image.save(image_path)
+
+        register_openers()
+        datagen, headers = multipart_encode({"photo": open(image_path, "rb")})
+
+        # Create the Request object
+        request = urllib2.Request(upload_url, datagen, headers)
+        # Actually do the request, and get the response
+        response = urllib2.urlopen(request).read()
+        return HttpResponse(response)
+
+    return HttpResponseBadRequest()
+
+@never_cache
+def set_fb(request):
+
+        data = request.POST.get('data')
+
+        try:
+            data = simplejson.loads(data)
+        except ValueError:
+            data = None
+
+        gender = request.POST.get('gender')
+        image = compose_image(data, gender)
+
+        image_filename = "%s.%s" % (str(uuid.uuid4()), 'png')
+        image_path = "%s/%s" % (settings.MEDIA_ROOT, image_filename)
+        image.save(image_path)
+
+        access_token = request.POST.get('token')
+
+        if not access_token:
+            user = facebook.get_user_from_cookie(request.COOKIES, settings.FB_APP_ID, settings.FB_APP_SECRET)
+            access_token = user["access_token"]
+
+        if access_token:
+            graph = facebook.GraphAPI(access_token)
+            response = graph.put_photo(open(image_path))
+            return HttpResponse(simplejson.dumps(response))
+        return HttpResponseBadRequest()
+
+
+def vk_frame(request):
+    return render_to_response('constructor/iframe.html',
+            {'VK_APP_ID': settings.VK_APP_ID},
+            context_instance=RequestContext(request))
