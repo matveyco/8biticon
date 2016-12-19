9e086bbb3337697f154997992523540f0eeb85f9
Roman Safonow <r.safonow@gmail.com>
2016-12-19 15:56:48 +0200

    add new coffee page

---
+++ public/static/javascripts/emails.js
@@ -0,0 +1,30 @@
+function sender() {
+	var email = $('#emailfield').val();
+	var name = $('#namefield').val();
+
+	if(!email) {
+		alert("Please enter email");
+		return;
+	}
+	if(!name) {
+		alert("Please enter name");
+		return;
+	}
+	if(!validateEmail(email)) {
+		alert("Enter correct e-mail");
+		return;
+	}
+
+	function validateEmail(email) {
+	    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	    return re.test(email);
+	}
+
+	var data = {email: email,
+				name: name};
+	$.post( "/emailswriter", data, function( data ) {
+	  if (data == 1) {
+	  	alert("Your e-mail was saved")
+	  }
+	})
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+var file = "output.txt";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,5 +24,39 @@ router.get('/privacy', function(req, res, next) {
 router.get('/terms', function(req, res, next) {
   res.render('terms');
 });
+
+router.post('/emailswriter', function(req, res, next) {
+	var body = req.body.name + ": " + req.body.email;
+    fs.exists(file, function (exists) {
+        if(!exists) { 
+        	create(body); 
+        	res.send('1')
+        }
+    	else {
+    		rewrite(body, res);
+    		res.send('1')
+        }
+	}); 
+})
+
+function create(str){
+	fs.writeFile(file, str, function (err) {
+        if (err) throw err;
+        console.log('It\'s saved! in same location.');
+    });
+}
+
+function rewrite(str, res){
+	data = str + "\r\n";
+
+	fs.readFile(file, 'utf8', function (err, output) {
+		if(err) console.log(err);
+
+		fs.appendFile(file, data, function (err) {
+			if(err) console.log(err);
+		});
+
+	});
+}
 
 module.exports = router;
